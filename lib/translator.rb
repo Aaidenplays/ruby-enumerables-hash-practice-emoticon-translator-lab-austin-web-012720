311c61f8ff42c3e4861b60ba8aee9b5aba57a71b
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-10 04:59:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,10 +11,10 @@ def load_library(file)
   file.each do |key, array|
               array.each do |array,value| 
                           hash[:get_meaning][]
-                          
+                          binding.pry
                         end
             end
-   binding.pry
+   
 hash
 end
 
