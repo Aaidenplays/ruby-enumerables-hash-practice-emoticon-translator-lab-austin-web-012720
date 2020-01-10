669a36c98e33a3c94bb3a07b135270e7e9193488
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-10 04:53:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,12 +4,12 @@ require 'pry'
 
 def load_library(file)
   file = YAML.load_file(file)
-  # file[:get_meaning] = {}
-  # file.each do |key, array|
-  #             array.each do |array,value| 
-  #                         file[:get_meaning] = key
-  #                       end
-  #           end
+  file[:get_meaning] = {}
+  file.each do |key, array|
+              array.each do |array,value| 
+                          file[:get_meaning] = key
+                        end
+            end
   #binding.pry
   hash =  {
     get_meaning: {},
