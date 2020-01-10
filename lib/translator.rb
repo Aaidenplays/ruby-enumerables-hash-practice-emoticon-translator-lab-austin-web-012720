ae00b54086602cc818eceea5db7347bf86d47324
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-10 05:25:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ end
 
 def get_japanese_emoticon(file , emote)
   emotes = load_library(file)
-  if emotes[:get_emoticon].detect{|n| n == emote}
+  if !emotes[:get_emoticon].has_key(emote)
     return "Sorry, that emoticon was not found"
   end
   emotes[:get_emoticon][emote]
