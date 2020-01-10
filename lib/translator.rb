4ba7331530ea0a706dfeda8a59f89b8795fbd7cc
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-10 05:29:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,9 +28,9 @@ def get_japanese_emoticon(file , emote)
 end
 
 def get_english_meaning(file,emote)
-   emotes = load_library(file)
-     binding.pry
-   if !emotes[:get_emoticon].has_key?(emote)
+  emotes = load_library(file)
+  binding.pry
+  if !emotes[:get_emoticon].has_key?(emote)
     return "Sorry, that emoticon was not found"
   else
   return emotes[:get_emoticon][emote]
