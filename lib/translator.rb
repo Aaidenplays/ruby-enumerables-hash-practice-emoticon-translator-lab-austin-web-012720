e58acca6fe6f4006ffc083870df721d8248b1936
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-10 04:53:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,17 +4,17 @@ require 'pry'
 
 def load_library(file)
   file = YAML.load_file(file)
-  
+    hash =  {
+    get_meaning: {},
+    get_emoticon: {}
+          }
   file.each do |key, array|
               array.each do |array,value| 
                           file[:get_meaning] = key
                         end
             end
   #binding.pry
-  hash =  {
-    get_meaning: {},
-    get_emoticon: {}
-          }
+
 end
 
 def get_japanese_emoticon
