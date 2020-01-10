b43119c2a66e40b5dccc09dc08bb8ba609b64ef1
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-10 04:41:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ def load_library(file)
   #                         hash[:get_meaning] = key
   #                       end
   #           end
-  #binding.pry
+  binding.pry
 end
 
 def get_japanese_emoticon
