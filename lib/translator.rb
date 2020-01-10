52818e98392ffb175829f6ca2d02457126d17d25
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-10 04:32:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,23 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file)
+  hash = {}
+  file = YAML.load_file(file)
+  # file.each do |key, array|
+  #             array.each do |array,value| 
+  #                         hash[:get_meaning] = (key)
+  #                       end
+  #           end
+  binding.pry
 end
 
 def get_japanese_emoticon
   # code goes here
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file)
+  load_library(file)
+  binding.pry
 end
